c34f689dd2ffaa4a52609c59ff73e924205f4abd
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-09-09 18:18:06 +0300

    Update lang-lit.py

--- lang-lit.py
+++ lang-lit.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import bs4
+import os
+
+
+API_KEY=st.secrets["openAI_key"]
+P_API_KEY =st.secrets["pincone_key"]
+
+
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
@@ -20,8 +26,8 @@ data = loader.load()
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter = RecursiveCharacterTextSplitter(
     # Set a really small chunk size, just to show.
-    chunk_size = 100,
-    chunk_overlap  = 20,
+    chunk_size = 10000,
+    chunk_overlap  = 500,
     length_function = len,
     add_start_index = True,
 )
@@ -30,16 +36,28 @@ from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
 import os
-embeddings = OpenAIEmbeddings(openai_api_key ="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") # set openai_api_key = 'your_openai_api_key'
-pinecone.init(api_key="5141cd92-aadf-4521-8819-ef21310d2722", environment="gcp-starter")
+embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
+pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
 index_name = pinecone.Index('index-1')
 from langchain.chat_models import ChatOpenAI
-llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key = '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')
-prediction = llm.predict(str(prompt))
-print (prediction)
+llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
+llm.predict(str(prompt))
+
+
+from langchain.chains import RetrievalQA
+vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
+retriever = vectordb.as_retriever()
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages= True)
+chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory= memory)
+query = str(prompt)
+Answer=chain.run({'question': query})
+
 
 with st.chat_message("user"):
-    st.write(str(prediction))
+    st.write(str(Answer))
 
 
 
